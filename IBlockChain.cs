6aa383d2f58a07a9bb67263ff645345d4138053f
RivkaAltshuler <32875275+RivkaAltshuler@users.noreply.github.com>
2018-08-16 23:36:38 +0300

    upload the blockChain interface

---
+++ IBlockChain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SecurityCourse.Exercise.Console
+{
+    public interface IBlockChain
+    {
+        void Add(Data data);
+        IEnumerator<Block> GetEnumerator();
+        void Verify();
+    }
+}
